d98953c89229a8505fe34666fb790eb656190ef0
Irishka <irishka66@gmail.com>
2016-02-08 19:13:20 +0300

    with node, without remove

--- binary-tree.js
+++ binary-tree.js
@@ -1,119 +1,112 @@
 'use strict';
 
-
-import Node from './node';
-
-/*class Node {
-	constructor(data, left, right) {
-		this.data = data || null;
-		this.left = left || null;
-		this.right = right || null;
-	}
-}*/
-
+//import Node from './node';
 
 class BinaryTree {
 	constructor() {
-        this.root = null;    
-   	}
+    this.root = null;
+	}
 
 	insert(data) {
-      
-      var currentNode = this.root;
-      
-      (function insertData(data){
-      
-        if (currentNode == null){
-        currentNode.data = data;
-      	currentNode.left = null;
-      	currentNode.right = null;
-        } else {
-      	  if (currentNode.data > data){
-               
-               currentNode = currentNode.left;
-               insertData(data);
 
-      	  } else {
-      	  	if (currentNode.data < data){
-      	     
-      	     currentNode = currentNode.right;
-               insertData(data);
+	  // if (this.root === null) {
+   //         this.root = new Node (data, null, null);
+   //         return;
+	  //  }
 
-      	  	} else {
-      	  		alert ("Node with " + data + " exists")
-      	  	}
-      	  }
-        }
-      }())
+	   
+	   //        class Node {
+				// 	constructor(data, left, right) {
+				// 		this.data = data || null;
+				// 		this.left = left || null;
+				// 		this.right = right || null;
+				// 	}
+				// }
+
+              var currentNode = this.root;
+
+	          function insertData(nodeData){
+	              if (!currentNode){
+	              currentNode = new Node (nodeData);
+	              return; 
+	              } else {
+	                    if (currentNode.data > nodeData){
+	                         currentNode = currentNode.left;
+	                         insertData(nodeData);
+	                    } else {
+	                          if (currentNode.data < nodeData){
+	                               currentNode = currentNode.right;
+	                               insertData(nodeData);
+	                          }else {
+	                              return; 
+	                          }
+	                    }
+	              }
+	            }
+
+	    insertData(data);
+	    return; // надо ли писать ретурн?
 
 	}
 
 	contains(data) {
 
-     var currentNode = this.root;
-      
-      (function containsData(data){
-      
-        if (currentNode.data == data){
-         	return true;
-        } else {
-      	  if (currentNode.data > data){
-               
-               currentNode = currentNode.left;
-               containsData(data);
-
-      	  } else {
-      	  	if (currentNode.data < data){
-      	     
-      	     currentNode = currentNode.right;
-               containsData(data);
-
-      	  	} else {
-      	  		alert ("Node with " + data + " doesn't exist")
-      	  	}
-      	  }
-        }
-      }()) 
-
+	     var currentNode = this.root;
+	        function containsData(nodeData){
+	            if (currentNode.data == nodeData){
+	              return true;
+	            } else {
+	                  if (currentNode.data > nodeData){
+	                       currentNode = currentNode.left;
+	                       containsData(nodeData);
+	                  } else {
+	                        if (currentNode.data < nodeData){
+	                           currentNode = currentNode.right;
+	                           containsData(nodeData);
+	                        } else {
+	                              return false;
+	                        }
+	                  }
+	            }
+	        }
+	     
+	     return containsData(data);
 
 	}
 
 	remove(data) {
 
+
+
 	}
 
 	size() {
+
       var currentNode = this.root;
       var counter = 0;
 
-      (function sizeData(){
-      
-         if (currentNode.data){
-         	     counter++;
-         } 
-         if (currentNode.left.data){    
-               currentNode = currentNode.left;
-               sizeData();
-         } 
-         if (currentNode.right.data){    
-               currentNode = currentNode.right;
-               sizeData();
-         } 
-
-         return counter;
-    	  
-      }()) 
-
-
-
+	      function sizeData(currentNode){
+	         if (currentNode.data){
+	         	     counter++;
+	         }	     
+	         if (currentNode.left.data){    
+     	             sizeData(currentNode.left);
+	         } 
+	         if (currentNode.right.data){    
+	                 sizeData(currentNode.left);
+	         }
+           return counter;
+	      }
+    
+      return sizeData(currentNode);
 
 	}
 
 	isEmpty() {
- 
-    if (this.root) {
-    	return true;
-    }
+      if (this.root) {
+    	return false;
+      } else {
+    	return true;}
 
 	}
 }
--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 'use strict';
-
-export default class Node {
+//export default
+ class Node {
 	constructor(data, left, right) {
 		this.data = data || null;
 		this.left = left || null;
